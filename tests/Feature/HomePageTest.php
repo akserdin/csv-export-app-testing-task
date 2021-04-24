8335ee9c4b7bca7b01ed278d0740a3ef1b6ec028
Dmytro Zolotar <dmytrozolotar@gmail.com>
2021-04-24 16:56:05 +0300

    Add home page test

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -2,20 +2,20 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class HomePageTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_if_home_page_works()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertSee('Completed testing task');
     }
 }
