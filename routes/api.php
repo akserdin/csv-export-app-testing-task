d2144b7734b25f4ae06422e94e745f6d19746125
Dmytro Zolotar <dmytrozolotar@gmail.com>
2021-04-24 16:55:26 +0300

    Name route

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::patch('csv-export', 'CsvExportController@download');
+Route::patch('csv-export', 'CsvExportController@download')
+    ->name('csv.download');
