efee0b6c6ac5a697f88650843b3b797d0f6ea130
Dmytro Zolotar <dmytrozolotar@gmail.com>
2021-04-24 15:02:13 +0300

    Refactor imports

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-        </style>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <title>CSV exporter testing task</title>
     </head>
     <body>
         <div id="app">
@@ -41,15 +24,10 @@
                     </div>
                 </div>
             </div>
-            <c-s-v-generator></c-s-v-generator>
+
+            <csv-generator :column-limit="5"></csv-generator>
         </div>
 
-        <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+        <script src="{{ mix('/js/app.js') }}" type="text/javascript"></script>
     </body>
 </html>
-<script>
-    import CSVGenerator from "../js/components/CSVGenerator";
-    export default {
-        components: {CSVGenerator}
-    }
-</script>
